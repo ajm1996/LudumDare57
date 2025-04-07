a63bee3a636b5c9690ca9b825c70bdeeb7c80d7b
Austin Miller <austinjmiller96@gmail.com>
2025-04-07 16:24:55 -0400

    added colored battery lights that turn off with their indictators

--- Assets/Scripts/FuelManager.cs
+++ Assets/Scripts/FuelManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class FuelManager : MonoBehaviour
@@ -16,6 +17,9 @@ public class FuelManager : MonoBehaviour
     [Header("Battery Sprites")]
     [SerializeField] private Sprite[] batterySprites; // Array of 6 battery sprites
     [SerializeField] private SpriteRenderer batterySpriteRenderer; // SpriteRenderer for the battery
+
+    [Header("Battery Lights")]
+    [SerializeField] private Light2D[] batteryLights; // Array of 5 lights corresponding to battery levels
 
     private bool isBlinking = false;
 
@@ -37,8 +41,8 @@ public class FuelManager : MonoBehaviour
         // Decrease fuel over time
         fossilFuelLevel -= passiveFuelRate * Time.deltaTime;
 
-        // Update the battery sprite based on the fuel level
-        UpdateBatterySprite();
+        // Update the battery sprite and lights based on the fuel level
+        UpdateBatterySpriteAndLights();
 
         // Restart the game if fuel is depleted
         if (fossilFuelLevel <= 0)
@@ -57,9 +61,9 @@ public class FuelManager : MonoBehaviour
         fossilFuelLevel = Mathf.Max(fossilFuelLevel - fuelAmount, 0);
     }
 
-    private void UpdateBatterySprite()
+    private void UpdateBatterySpriteAndLights()
     {
-        if (batterySprites == null || batterySprites.Length < 6 || batterySpriteRenderer == null)
+        if (batterySprites == null || batterySprites.Length < 6 || batterySpriteRenderer == null || batteryLights == null || batteryLights.Length < 5)
             return;
 
         float fuelPercentage = fossilFuelLevel / fossilFuelMax;
@@ -67,26 +71,31 @@ public class FuelManager : MonoBehaviour
         if (fuelPercentage > 0.8f)
         {
             batterySpriteRenderer.sprite = batterySprites[0]; // 100% - 81%
+            UpdateBatteryLights(5); // All lights on
             isBlinking = false;
         }
         else if (fuelPercentage > 0.6f)
         {
             batterySpriteRenderer.sprite = batterySprites[1]; // 80% - 61%
+            UpdateBatteryLights(4); // 4 lights on
             isBlinking = false;
         }
         else if (fuelPercentage > 0.4f)
         {
             batterySpriteRenderer.sprite = batterySprites[2]; // 60% - 41%
+            UpdateBatteryLights(3); // 3 lights on
             isBlinking = false;
         }
         else if (fuelPercentage > 0.2f)
         {
             batterySpriteRenderer.sprite = batterySprites[3]; // 40% - 21%
+            UpdateBatteryLights(2); // 2 lights on
             isBlinking = false;
         }
         else if (fuelPercentage > 0.05f)
         {
             batterySpriteRenderer.sprite = batterySprites[4]; // 20% - 6%
+            UpdateBatteryLights(1); // 1 light on
             isBlinking = false;
         }
         else
@@ -99,14 +108,39 @@ public class FuelManager : MonoBehaviour
         }
     }
 
+    private void UpdateBatteryLights(int activeLights)
+    {
+        for (int i = 0; i < batteryLights.Length; i++)
+        {
+            if (i < activeLights)
+            {
+                batteryLights[i].enabled = true; // Turn on the light
+            }
+            else
+            {
+                batteryLights[i].enabled = false; // Turn off the light
+            }
+        }
+    }
+
     private System.Collections.IEnumerator BlinkBattery()
     {
         isBlinking = true;
-        while (fossilFuelLevel / 100f <= 0.05f)
+        while (fossilFuelLevel / fossilFuelMax <= 0.05f)
         {
+            // Blink the battery sprite
             batterySpriteRenderer.sprite = batterySprites[4]; // Second to last sprite
+            if (batteryLights.Length > 0)
+            {
+                batteryLights[0].enabled = true; // Turn on the last light
+            }
             yield return new WaitForSeconds(0.5f);
+
             batterySpriteRenderer.sprite = batterySprites[5]; // Last sprite
+            if (batteryLights.Length > 0)
+            {
+                batteryLights[0].enabled = false; // Turn off the last light
+            }
             yield return new WaitForSeconds(0.5f);
         }
         isBlinking = false;
